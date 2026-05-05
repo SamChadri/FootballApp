051e1e0bf3acaa8dacb275a9b51f85b1914058b7
Samuel Chadri <samiam@Samuels-MacBook-Pro.local>
2026-05-05 16:52:05 -0500

    fix: register data pages via Routing.RegisterRoute instead of ShellContent

--- src/Football.MauiApp/AppShell.xaml.cs
+++ src/Football.MauiApp/AppShell.xaml.cs
@@ -2,10 +2,14 @@ namespace Football.MauiApp;
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
+    public AppShell()
+    {
+        InitializeComponent();
 
-        Routing.RegisterRoute(nameof(Views.AddGamePage), typeof(Views.AddGamePage));
-	}
+        Routing.RegisterRoute(nameof(Views.AddGamePage),     typeof(Views.AddGamePage));
+        Routing.RegisterRoute(nameof(Views.DataPage),        typeof(Views.DataPage));
+        Routing.RegisterRoute(nameof(Views.TeamGroupPage),   typeof(Views.TeamGroupPage));
+        Routing.RegisterRoute(nameof(Views.PositionPage),    typeof(Views.PositionPage));
+        Routing.RegisterRoute(nameof(Views.PlayerStatsPage), typeof(Views.PlayerStatsPage));
+    }
 }
